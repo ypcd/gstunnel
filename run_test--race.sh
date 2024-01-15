302c98600dc1aacbc11eb0fd1e764bb4b8c50d2c
ypcd <mingzyun14328@outlook.com>
2024-01-16 04:32:40 +0800

    Delete run_test--race.sh

--- run_test--race.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export GO111MODULE="auto"
-#export GOPROXY="https://goproxy.io,direct"
-export gorace="log_path=."
-
-go mod tidy
-go get -u ./...
-go test -race -timeout 0 -p 1 -cover ./...
